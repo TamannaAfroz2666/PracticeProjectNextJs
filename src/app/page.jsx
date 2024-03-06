55e5b9bcd16ca78669e7d68eda23090de5d8abf9
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-03-06 12:38:40 +0600

    page new updated starting

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,32 @@
 
+import Link from 'next/link';
 import '../Styles/Home.css'
-import Base from './components/page';
-import Dashboard from './dashboard/page';
+// import Base from './components/page';
+// import Dashboard from './dashboard/page';
 // import Appto from './appto/Appto';
 
 export default function Home() {
   return (
     <main className=" min-h-screen ">
-      <h1 className="tagName">
+      <div className="containerMain">
+        <div className="navContainer">
+          <ul className='navContainerUl'>
+            <li>
+              <Link className='relevantTitle' href="/relevant">Relevant</Link>
+              </li>
+            <li>
+              <Link  className='linkTitle' href="/latest">Latest</Link>
+              </li>
+            <li>
+              <Link className='linkTitle' href="/top">top</Link>
+              </li>
+          </ul>
+        </div>
+        <h1 className="tagName">
         Home page
       </h1>
+      </div>
+     
     </main>
   );
 }
