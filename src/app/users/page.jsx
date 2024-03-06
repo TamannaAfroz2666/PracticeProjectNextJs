790655e7805bec323c698bbd3634dd6489e9aa04
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-03-06 10:40:50 +0600

    add replace and prefetch method

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -4,7 +4,14 @@ import React from 'react';
 const Users = () => {
     return (
         <div className=" min-h-screen ">
-            <Link href="/users/1" >user1</Link> 
+            <Link href={{
+                pathname:"/users/1",
+                query:{
+                    isAdmin: false
+
+                }
+
+            }}replace prefetch={false} >user1</Link> 
             <Link href="/users/2" >user2</Link>
             <Link href="/users/3" >user3</Link>
             <Link href="/users/4" >user4</Link>
